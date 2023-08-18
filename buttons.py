1e294814c523aff33b0807a4d5f2fdc03d034bd6
Vlad <vmusaelyae@gmail.com>
2023-08-18 13:13:13 +0300

    Second commit, added downloading video from YouTube and some buttons, need to add tests

--- buttons.py
+++ buttons.py
@@ -1,11 +1,23 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-b1 = KeyboardButton("/help")
-b2 = KeyboardButton("/description")
-b3 = KeyboardButton("/image")
-b4 = KeyboardButton("/location")
-b5 = KeyboardButton("❤️")
-b6 = KeyboardButton("/download_vid")
-kb.insert(b1).insert(b2).insert(b3).insert(b4).insert(b5).insert(b6)
+# Close Keyboard
+close_kb = ReplyKeyboardRemove()
+
+# Start Keyboard
+kb_main = ReplyKeyboardMarkup(resize_keyboard=True)
+b1 = KeyboardButton("📝 Існтрукція")
+b2 = KeyboardButton("📜 Опис Боту")
+b3 = KeyboardButton("🎼 Завантажити пісню")
+b4 = KeyboardButton("🎥 Завантажити відео")
+kb_main.insert(b1).insert(b2).insert(b3).insert(b4)
+
+
+# Keyboard for choosing resolution when user downloading video from YouTube
+kb_resolution = ReplyKeyboardMarkup(resize_keyboard=True)
+b1_res = KeyboardButton("360p")
+b2_res = KeyboardButton("720p")
+b3_res = KeyboardButton("1080p")
+b4_res = KeyboardButton("The best res")
+kb_resolution.insert(b1_res).insert(b2_res).insert(b3_res).insert(b4_res)
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from io import BytesIO
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
@@ -19,54 +20,74 @@ dp = Dispatcher(bot, storage=storage)
 
 class Form(StatesGroup):
     WAITING_FOR_TEXT = State()
+    WAITING_FOR_QUALITY = State()
 
 
-@dp.message_handler(commands=["start"])
+@dp.message_handler(lambda message: message.text == "/start")
 async def get_start(message: types.Message):
     await message.answer(text=START_TEXT,
-                         reply_markup=kb)
+                         reply_markup=kb_main)
 
-@dp.message_handler(commands=["description"])
+@dp.message_handler(lambda message: message.text == "📜 Опис Боту")
 async def get_descr(message: types.Message):
     await message.answer(text=DESCRIPTION_TEXT)
-    await message.delete()
 
 
-@dp.message_handler(commands=["help"])
+@dp.message_handler(lambda message: message.text == "📝 Існтрукція")
 async def get_help(message: types.Message):
     await message.answer(text=TEXT_HELP)
 
-@dp.message_handler(commands=["image"])
+@dp.message_handler(lambda message: message.text == "🎼 Завантажити пісню")
 async def get_image(message: types.Message):
     await bot.send_photo(chat_id=message.from_user.id,
                          photo="https://w.forfun.com/fetch/32/3206bb3089350c851170b2c5ac00a6e8.jpeg"
                          )
-    await message.delete()
 
 
-@dp.message_handler(commands=["download_vid"])
+@dp.message_handler(lambda message: message.text == "🎥 Завантажити відео")
 async def start_download(message: types.Message):
-    await Form.WAITING_FOR_TEXT.set()  # Set the state to waiting for text input
-    await message.answer("Надішліть посилання на відео яке хочете завантажити")
+    await Form.WAITING_FOR_TEXT.set()
+    await message.answer("Надішліть посилання на відео яке хочете завантажити", reply_markup=close_kb)
 
 
-@dp.message_handler(state=Form.WAITING_FOR_TEXT)  # Handle the user's response in this state
+@dp.message_handler(lambda message: message.text.startswith("http"), state=Form.WAITING_FOR_TEXT)
 async def process_text(message: types.Message, state: FSMContext):
-    url_youtube = message.text
+    await Form.WAITING_FOR_QUALITY.set()  # Устанавливаем состояние ожидания выбора качества
 
-    try:
-        video = YouTube(url_youtube)
-        stream = video.streams.get_highest_resolution()
-        buffer = BytesIO()
-        stream.stream_to_buffer(buffer)
+    async with state.proxy() as data:
+        data['url'] = message.text
 
-        buffer.seek(0)  # Reset buffer position to the beginning
-        await bot.send_video(message.chat.id, buffer)
-        await state.finish()
-        await message.reply("Ваше відео у найкращій якості !!!")
-    except Exception as e:
-        await message.reply(f"Помилка завантаження: {e}")
-        await state.finish()
+    await message.answer(text='Оберіть якість відео яке хочете завантажити:', reply_markup=kb_resolution)
+
+
+@dp.message_handler(lambda message: message.text in ["360p", "720p", "1080p", "The best res"], state=Form.WAITING_FOR_QUALITY)
+async def process_quality_choice(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        url_youtube = data['url']
+        user_choice = message.text
+
+        try:
+            video = YouTube(url_youtube)
+            if user_choice != "The best res":
+                stream = video.streams.get_by_resolution(user_choice)
+            else:
+                stream = video.streams.get_highest_resolution()
+            buffer = BytesIO()
+            stream.stream_to_buffer(buffer)
+
+            start_message = await message.answer("⚒ Йде завантаження відео...")
+
+            buffer.seek(0)  # Сбрасываем позицию буфера в начало
+            await bot.send_video(message.chat.id, buffer)
+
+            await bot.delete_message(message.chat.id, start_message.message_id)
+
+            await state.finish()
+            await message.reply("✅ Ваше відео у вибраній якості завантажено", reply_markup=kb_main)
+        except Exception as e:
+            await message.reply(f"❌ Помилка завантаження: {e} ❌")
+            await state.finish()
+
 
 
 if __name__ == "__main__":
